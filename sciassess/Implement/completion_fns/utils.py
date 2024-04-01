6097c805cefe9bbecf9ea740b81cba701d085b54
caixiaochen <caixc97@gmail.com>
2024-04-01 16:47:31 +0800

    version 0.9.2

--- sciassess/Implement/completion_fns/utils.py
+++ sciassess/Implement/completion_fns/utils.py
@@ -65,6 +65,18 @@ def extract_text(pdf_path, add_page_num: bool = False) -> list[str]:
             texts.append(text)
     return texts
 
+def call_without_throw(func):
+    # If there is an error while calling, log the error and return the error message, rather than throwing an exception
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            result = ErrorCompletionResult(exception=traceback.format_exc())
+        return result
+    return wrapper
+
 class ErrorCompletionResult(CompletionResult):
     def __init__(self, exception) -> None:
         self.exception = exception
---
+++ sciassess/Implement/utils/__init__.py
@@ -0,0 +1,2 @@
+from pathlib import Path
+PROJECT_PATH = Path(__file__).parents[3]
